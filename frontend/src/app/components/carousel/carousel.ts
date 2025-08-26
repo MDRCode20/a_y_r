0f207c7cecdd48fb68a7900790b29bcc1762fc5c
MDRCode20 <milagrosdayanarosalesapaza@example.com>
2025-08-25 23:55:50 -0500

    Primer commit - proyecto con carpeta frontend

--- frontend/src/app/components/carousel/carousel.ts
+++ frontend/src/app/components/carousel/carousel.ts
@@ -1,16 +1,47 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  animateChild,
+  group
+} from '@angular/animations';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.html',
-  styleUrls: ['./carousel.css']
+  styleUrls: ['./carousel.css'],
+  animations: [
+    trigger('carouselAnimation', [
+      transition('* => *', [
+        // Estado inicial de la nueva imagen
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateX(100%)' })
+        ], { optional: true }),
+
+        // Estado final de la imagen que se va
+        query(':leave', [
+          style({ opacity: 1, transform: 'translateX(0)' }),
+          animate('600ms ease', style({ opacity: 0, transform: 'translateX(-100%)' }))
+        ], { optional: true }),
+
+        // Animación de entrada de la nueva imagen
+        query(':enter', [
+          animate('600ms ease', style({ opacity: 1, transform: 'translateX(0)' }))
+        ], { optional: true }),
+      ])
+    ])
+  ]
 })
 export class CarouselComponent {
- images: string[] = [
-  'assets/images/img1.jpg',
-  'assets/images/img2.jpg',
-  'assets/images/img3.jpg'
-];
+  images = [
+    'assets/img1.jpg',
+    'assets/img2.jpg',
+    'assets/img3.jpg'
+  ];
+
   currentIndex = 0;
 
   next() {
@@ -18,6 +49,7 @@ export class CarouselComponent {
   }
 
   prev() {
-    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) % this.images.length;
   }
 }
